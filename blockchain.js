1dfad5f2e8ab36381e9d72717f06b455da1269bb
Renkon117 <randyok555@gmail.com>
2019-10-06 13:25:01 -0500

    created replace chain method

--- blockchain.js
+++ blockchain.js
@@ -32,6 +32,21 @@ class Blockchain {
 
         return true;
     }
+
+    replaceChain(chain) {
+        if (chain.length <= this.chain.length) {
+            console.error('The incoming chain must be longer');
+            return;
+        }
+
+        if (!Blockchain.isValidChain(chain)) {
+            console.error('The incoming chain must be valid');
+
+            return;
+        }
+        console.log('replacing chain with', chain);
+        this.chain = chain;
+    }
 }
 
 module.exports = Blockchain;
