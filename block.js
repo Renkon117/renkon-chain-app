e496bc7a514592676f49a5747a2ff9a550f4e328
Renkon117 <randyok555@gmail.com>
2019-10-05 22:26:44 -0500

    added blockchain class and add function

--- block.js
+++ block.js
@@ -1,5 +1,6 @@
 
 const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
 
 
 class Block {
@@ -16,7 +17,10 @@ class Block {
     }
 
     static mineBlock({ lastBlock, data }) {
-        return new this({ timestamp: Date.now(), lastHash: lastBlock.hash, data })
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+
+        return new this({ timestamp, lastHash, hash: cryptoHash(timestamp, lastHash, data), data })
     }
 }
 
